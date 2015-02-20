f1910eac57850f6e9e819d24305ee06fedff0f10
Francis De Brabandere <francisdb@gmail.com>
2015-02-20 10:26:29 +0100

    added script to generate pdfs

---
+++ topdf.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+function installPandocIfNeeded {
+  echo "checking if pandoc is installed..."
+  if command -v pandoc > /dev/null 2>&1 ; then
+    echo "...ok"
+  else
+    installPandoc
+  fi  
+}
+
+function installPandoc {
+  echo "installing pandoc"
+  if [[ $OSTYPE == darwin* ]]; then
+    sudo port install pandoc pdflatex
+  else
+    sudo apt-get install pandoc pdflatex
+  fi
+}
+
+installPandocIfNeeded
+
+git clone https://github.com/waylayio/documentation.wiki.git
+git clone https://github.com/waylayio/WaylayPlugins.wiki.git
+ls -al documentation.wiki
+ls -al WaylayPlugins.wiki
+
+pandoc -o waylay-api.pdf documentation.wiki/Waylay-REST-API-documentation.md
+pandoc -o waylay-broker.pdf documentation.wiki/Submitting-and-fetching-data.md
+pandoc -o waylay-plugins.pdf WaylayPlugins.wiki/Plugin-API.md
+
+rm -rf documentation.wiki
+rm -rf WaylayPlugins.wiki

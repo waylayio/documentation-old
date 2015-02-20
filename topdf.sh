3200402fb8f0a83fcdc9dbfbcdd30c4f0b44475c
Francis De Brabandere <francisdb@gmail.com>
2015-02-20 12:22:57 +0100

    tried markdown-pdf but dus not yield any better results

--- topdf.sh
+++ topdf.sh
@@ -13,22 +13,46 @@ installPandocIfNeeded () {
 installPandoc () {
   echo "installing pandoc"
   if [[ $OSTYPE == darwin* ]]; then
-    sudo port install pandoc pdflatex
+    sudo port install pandoc
   else
-    sudo apt-get install pandoc pdflatex
+    sudo apt-get install pandoc texlive-latex-base texlive-fonts-recommended texlive-latex-extra
   fi
 }
 
+installMarkdownPdf () {
+  echo "installing markdown-pdf"
+  npm install markdown-pdf
+}
+
+convertWithPandoc() {
+  echo "converting with pandoc..."
+  pandoc -o waylay-api.pdf documentation.wiki/Waylay-REST-API-documentation.md
+  pandoc -o waylay-broker.pdf documentation.wiki/Submitting-and-fetching-data.md
+  pandoc -o waylay-plugins.pdf WaylayPlugins.wiki/Plugin-API.md
+}
+
+convertWithMarkdownPdf () {
+  echo "converting with markdown-pdf..."
+  ./node_modules/.bin/markdown-pdf -o waylay-api.pdf documentation.wiki/Waylay-REST-API-documentation.md
+  ./node_modules/.bin/markdown-pdf -o waylay-broker.pdf documentation.wiki/Submitting-and-fetching-data.md
+  ./node_modules/.bin/markdown-pdf -o waylay-plugins.pdf WaylayPlugins.wiki/Plugin-API.md
+}
+
 installPandocIfNeeded
+#installMarkdownPdf
+
+rm *.pdf
+rm -rf documentation.wiki
+rm -rf WaylayPlugins.wiki
 
 git clone https://github.com/waylayio/documentation.wiki.git
 git clone https://github.com/waylayio/WaylayPlugins.wiki.git
-ls -al documentation.wiki
-ls -al WaylayPlugins.wiki
 
-pandoc -o waylay-api.pdf documentation.wiki/Waylay-REST-API-documentation.md
-pandoc -o waylay-broker.pdf documentation.wiki/Submitting-and-fetching-data.md
-pandoc -o waylay-plugins.pdf WaylayPlugins.wiki/Plugin-API.md
+convertWithPandoc
+#convertWithMarkdownPdf
 
 rm -rf documentation.wiki
 rm -rf WaylayPlugins.wiki
+
+echo "..done, pdf files created:"
+ls *.pdf

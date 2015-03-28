b3058314f433a64143a750cbb96741902ae5c95f
Veselin Pizurica <veselin.pizurica@gmail.com>
2015-03-28 22:54:25 +0100

    added HTML script

---
+++ toHTML.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+
+installPandocIfNeeded () {
+  echo "checking if pandoc is installed..."
+  if command -v pandoc > /dev/null 2>&1 ; then
+    echo "...ok"
+  else
+    installPandoc
+  fi  
+}
+
+installPandoc () {
+  echo "installing pandoc"
+  if [[ $OSTYPE == darwin* ]]; then
+    sudo port install pandoc
+  else
+    sudo apt-get install pandoc texlive-latex-base texlive-fonts-recommended texlive-latex-extra
+  fi
+}
+
+convertWithPandoc() {
+  echo "converting with pandoc..."
+  pandoc -s -S --toc -c -A documentation.wiki/Waylay-REST-API-documentation.md -o Waylay-REST-API-documentation.html
+  pandoc -s -S --toc -c -A documentation.wiki/Submitting-and-fetching-data.md -o Submitting-and-fetching-data.html
+  pandoc -s -S --toc -c -A WaylayPlugins.wiki/Plugin-API.md -o Plugin-API.html
+}
+
+convertToHTML(){
+  l=`grep -nr h1 Plugin-API.html | head -1 | cut -f2 -d':'`
+  tail -n+$l Plugin-API.html | sed 's/\[TOC\]//g'| sed -e :a -e '$d;N;2,2ba' -e 'P;D' | sed 's/.png"/.png" class="img-responsive" /g'  > temp.html
+  cat header.txt temp.html footer.txt > Plugin-API.html 
+
+  l=`grep -nr h1 Waylay-REST-API-documentation.html | head -1 | cut -f2 -d':'`
+  tail -n+$l Waylay-REST-API-documentation.html | sed 's/\[TOC\]//g' | sed -e :a -e '$d;N;2,2ba' -e 'P;D' | sed 's/.png"/.png" class="img-responsive" /g'   > temp.html
+  cat header.txt temp.html footer.txt > Waylay-REST-API-documentation.html
+
+  l=`grep -nr h1 Submitting-and-fetching-data.html | head -1 | cut -f2 -d':'`
+  tail -n+$l Submitting-and-fetching-data.html | sed 's/\[TOC\]//g' | sed -e :a -e '$d;N;2,2ba' -e 'P;D'  | sed 's/.png"/.png" class="img-responsive" /g' > temp.html
+  cat header.txt temp.html footer.txt > Submitting-and-fetching-data.html
+
+  rm temp.html
+}
+
+installPandocIfNeeded
+#installMarkdownPdf
+
+rm -f *.html
+rm -rf documentation.wiki
+rm -rf WaylayPlugins.wiki
+
+git clone https://github.com/waylayio/documentation.wiki.git
+git clone https://github.com/waylayio/WaylayPlugins.wiki.git
+
+convertWithPandoc
+convertToHTML
+
+rm -rf documentation.wiki
+rm -rf WaylayPlugins.wiki
+
+echo "..done, html files created:"
+ls *.html

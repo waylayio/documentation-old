06bb5943ebf42d850d127193152bf0f6b6a707b4
Francis De Brabandere <francisdb@gmail.com>
2016-07-04 14:11:13 +0200

    enable -H function for grep to always show filenames (difference between OSX and Linux)

--- toHTML.sh
+++ toHTML.sh
@@ -35,31 +35,31 @@ convertWithPandoc() {
 }
 
 convertToHTML(){
-  l=`grep -nr h1 "${WORKDIR}Plugin-API.html" | head -1 | cut -f2 -d':'`
+  l=`grep -nrH h1 "${WORKDIR}Plugin-API.html" | head -1 | cut -f2 -d':'`
   tail -n+$l "${WORKDIR}Plugin-API.html" | sed 's/\[TOC\]//g'| sed -e :a -e '$d;N;2,2ba' -e 'P;D' | sed 's/.png"/.png" class="img-responsive" /g' > "${WORKDIR}temp.html"
   cat header.txt "${WORKDIR}temp.html" footer.txt > "${WORKDIR}Plugin-API.html"
 
-  l=`grep -nr h1 "${WORKDIR}Waylay-REST-API-documentation.html" | head -1 | cut -f2 -d':'`
+  l=`grep -nrH h1 "${WORKDIR}Waylay-REST-API-documentation.html" | head -1 | cut -f2 -d':'`
   tail -n+$l "${WORKDIR}Waylay-REST-API-documentation.html" | sed 's/\[TOC\]//g' | sed -e :a -e '$d;N;2,2ba' -e 'P;D' | sed 's/.png"/.png" class="img-responsive" /g' > "${WORKDIR}temp.html"
   cat header.txt "${WORKDIR}temp.html" footer.txt > "${WORKDIR}Waylay-REST-API-documentation.html"
 
-  l=`grep -nr h1 "${WORKDIR}Submitting-and-fetching-data.html" | head -1 | cut -f2 -d':'`
+  l=`grep -nrH h1 "${WORKDIR}Submitting-and-fetching-data.html" | head -1 | cut -f2 -d':'`
   tail -n+$l "${WORKDIR}Submitting-and-fetching-data.html" | sed 's/\[TOC\]//g' | sed -e :a -e '$d;N;2,2ba' -e 'P;D'  | sed 's/.png"/.png" class="img-responsive" /g' > "${WORKDIR}temp.html"
   cat header.txt "${WORKDIR}temp.html" footer.txt > "${WORKDIR}Submitting-and-fetching-data.html"
 
-  l=`grep -nr h1 "${WORKDIR}Waylay-starters-guide.html" | head -1 | cut -f2 -d':'`
+  l=`grep -nrH h1 "${WORKDIR}Waylay-starters-guide.html" | head -1 | cut -f2 -d':'`
   tail -n+$l "${WORKDIR}Waylay-starters-guide.html"  | sed 's/\[TOC\]//g' | sed -e :a -e '$d;N;2,2ba' -e 'P;D'  | sed 's/.png"/.png" class="img-responsive" /g' > "${WORKDIR}temp.html"
   cat header.txt "${WORKDIR}temp.html" footer.txt > "${WORKDIR}Waylay-starters-guide.html"
 
-  l=`grep -nr h1 "${WORKDIR}Architecture.html" | head -1 | cut -f2 -d':'`
+  l=`grep -nrH h1 "${WORKDIR}Architecture.html" | head -1 | cut -f2 -d':'`
   tail -n+$l "${WORKDIR}Architecture.html"  | sed 's/\[TOC\]//g' | sed -e :a -e '$d;N;2,2ba' -e 'P;D'  | sed 's/.png"/.png" class="img-responsive" /g' > "${WORKDIR}temp.html"
   cat header.txt "${WORKDIR}temp.html" footer.txt > "${WORKDIR}Architecture.html"
 
-  l=`grep -nr h1 "${WORKDIR}Docker-image-guide.html" | head -1 | cut -f2 -d':'`
+  l=`grep -nrH h1 "${WORKDIR}Docker-image-guide.html" | head -1 | cut -f2 -d':'`
   tail -n+$l "${WORKDIR}Docker-image-guide.html"  | sed 's/\[TOC\]//g' | sed -e :a -e '$d;N;2,2ba' -e 'P;D'  | sed 's/.png"/.png" class="img-responsive" /g' > "${WORKDIR}temp.html"
   cat header.txt "${WORKDIR}temp.html" footer.txt > "${WORKDIR}Docker-image-guide.html"
 
-  l=`grep -nr h1 "${WORKDIR}Tasks-and-Templates.html" | head -1 | cut -f2 -d':'`
+  l=`grep -nrH h1 "${WORKDIR}Tasks-and-Templates.html" | head -1 | cut -f2 -d':'`
   tail -n+$l "${WORKDIR}Tasks-and-Templates.html" | sed 's/\[TOC\]//g' | sed -e :a -e '$d;N;2,2ba' -e 'P;D'  | sed 's/.png"/.png" class="img-responsive" /g' > "${WORKDIR}temp.html"
   cat header.txt "${WORKDIR}temp.html" footer.txt > "${WORKDIR}Tasks-and-Templates.html"
 
